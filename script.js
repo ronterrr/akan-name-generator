81536ed99fe762c36ace70940f21c98d91125d40
Daveny Kuria <davenykuria@gmail.com>
2026-05-03 21:09:12 +0300

    script(formula): add formula to script

--- script.js
+++ script.js
@@ -22,6 +22,34 @@ function onSubmit(event){
 
     let date = new Date(dobValue); //USER DATE OBJECT
 
+    // Formula
+
+    let stringYear = (date.getFullYear()).toString();
+
+    let mmDigit = (date.getMonth()+1);
+    let ddDigit = date.getDate();
+
+    if (mmDigit===1){
+        mmDigit=13;
+        stringYear = (Number(stringYear-1)).toString();
+    }else if (mmDigit===2){
+        mmDigit=14;
+        stringYear = (Number(stringYear-1)).toString();
+    }
+
+    let ccDigits = Number(`${stringYear[0]}${stringYear[1]}`);
+    let yyDigits = Number(`${stringYear[2]}${stringYear[3]}`);
+
+    // let formulaDayOfWeek = ((((ccDigits/4)-2*ccDigits-1) + ((5*yyDigits)/4) + ((26*(mmDigit+1))/10) + ddDigit)%7);
+
+    let firstEquation = Math.floor((ccDigits/4)-2*ccDigits-1);
+    let secondEquation = Math.floor((5*yyDigits)/4);
+    let thirdEquation = Math.floor((26*(mmDigit+1))/10);
+    let formulaDayOfWeek = ((((firstEquation + secondEquation + thirdEquation + ddDigit)%7)+7)%7);
+    console.log(formulaDayOfWeek);
+    console.log(date.getDay());
+
+
     // Validating entered data
 
     const dayOfWeek = days[date.getDay()];
